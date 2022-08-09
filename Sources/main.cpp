4b68713a4bae3f8e893e1346eba4fb4c27e5f064
RoboHoboMobo <0xrobohobomobo@gmail.com>
2022-08-09 15:15:44 +0300

    Refactoring

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -4,7 +4,7 @@
 #include "BinarySearchTree.h"
 
 template <typename T>
-bool isLeaf(const typename Custom::BinarySearchTree<T>::Node* node)
+bool isLeaf(const typename Custom::BinarySearchTreeNode<T>* node)
 {
     return node && !node->leftChild && !node->rightChild;
 }
@@ -14,10 +14,10 @@ std::vector<T> getLeafs(const Custom::BinarySearchTree<T>& bst)
 {
     std::vector<T> result;
 
-    typename Custom::BinarySearchTree<T>::iterator it = bst.cbegin();
-    for (; it != bst.cend(); ++it)
-        if (isLeaf<T>(it.get()))
-            result.push_back(*it);
+    for (auto it = bst.cbegin(); it != bst.cend(); ++it) {
+        if (isLeaf(it.get()))
+            result.emplace_back(*it);
+    }
 
     return result;
 }
@@ -29,7 +29,7 @@ void getLeafsRecursively(const typename Custom::BinarySearchTree<T>::Node* node,
     if (!node)
         return;
 
-    if (isLeaf<T>(node))
+    if (isLeaf(node))
         nodeList.push_back(node->data);
 
     getLeafsRecursively(node->leftChild, nodeList);
@@ -65,7 +65,7 @@ int main()
      *         1   3   11  17  32
      */
 
-    // const std::vector<int> leafs = getLeafs(bst);
+    const std::vector<int> leafs = getLeafs(bst);
 
     // for (auto leaf : leafs)
     //     std::cout << leaf << std::endl;
